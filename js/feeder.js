74e3f0c2131ff13d750a4968a7d61bdbf742979d
M. en C. Carlos Genaro <hyunkeln@gmail.com>
2014-01-29 12:55:50 -0600

    Agregado nivo slider

--- js/feeder.js
+++ js/feeder.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
   $.ajax({
 		url:'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&output=json_xml&num=100&callback=?&q='+ encodeURIComponent(url),
 		dataType: 'json',
-		success: parsePane
+		success: parseMainFeed
   });
   $.ajax({
 		url:"http://msn-rd.com/eservicios/json/bannerswg",
@@ -17,6 +17,11 @@ $(document).ready(function () {
 		success: parseBanners
   });
 });
+
+function parseMainFeed(data){
+	parsePane(data);
+	parseNivo(data);
+}
 
 function parsePane(data) {
 	data.responseData.xmlString = data.responseData.xmlString.replace(/msncp\:/g, 'msncp');
@@ -70,10 +75,10 @@ function parseEntries(data) {
 
 function parseBanners(data) {
 	
-	var placeHolder = "ul.banners";
+	var placeHolder = "div.banners";
 	var items = data.items;
-	var li = $(placeHolder+" li");
-	$(placeHolder+" li").remove();
+	var li = $(placeHolder+" .li");
+	$(placeHolder+" .li").remove();
 	for(var j=0;j<items.length;j++){
 		var item = items[j];
 		var itemimage = item.php;
@@ -82,7 +87,34 @@ function parseBanners(data) {
 		$(newli).find("a").attr("title",item.title);
 		$(newli).find("a").attr("href",item.link);
 		$(newli).find("img").attr("src",itemimage);
+		if(j==items.length-1) $(newli).addClass("last");
 		$(placeHolder).append(newli);
 	}
 	
+}
+
+function parseNivo(data) {
+	data.responseData.xmlString = data.responseData.xmlString.replace(/msncp\:/g, 'msncp');
+	var placeHolder = "#slider-nivo";
+	var items = data.responseData.feed.entries;
+	var xmlsrc = $(data.responseData.xmlString).find("item");
+	var li = $(placeHolder+" .li");
+	$(placeHolder+" .li").remove();
+	for(var j=0;j<items.length;j++){
+		var item = items[j];
+		var itemimage = $(xmlsrc[j]).find("enclosure").attr("url");
+		var newli = li.clone();
+		//$(newli).find("h2").text(item.title);
+		$(newli).find("a").attr("href",item.link);
+		$(newli).find("img").attr("src",itemimage);
+		$(newli).find("img").attr("data-thumb",itemimage);
+		$(placeHolder).append(newli);
+	}
+    $(placeHolder).nivoSlider({
+	    effect:'fade',
+	    slices:1,
+	    pauseTime: 10000,
+	    controlNavThumbs:false
+    });
+
 }
