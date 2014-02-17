c0b82028d0ed1dd4d680d0902b9619e4da9b6afb
M. en C. Carlos Genaro <hyunkeln@gmail.com>
2014-02-17 12:57:26 -0600

    Cambiados sliders y fuentes

--- js/feeder.js
+++ js/feeder.js
@@ -8,58 +8,29 @@ $(document).ready(function () {
 	$.ajax({
 		url:'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&output=json_xml&num=100&callback=?&q='+ encodeURIComponent(url),
 		dataType: 'json',
-		success: parseEntries
-  });
-  if(top.location.search.indexOf("usuarioConSesion")>-1) url = "http://estilos.prodigy.msn.com/rss-carrouselinfopane.aspx";//"http://prodigy.msn.com/rss-slideshow-telmex.aspx"; //Url con sesión
-  else url = "http://prodigy.msn.com/rss-slideshow-telmex.aspx"
-  $.ajax({
-		url:'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&output=json_xml&num=100&callback=?&q='+ encodeURIComponent(url),
-		dataType: 'json',
-		success: parsePane
+		success: parseCenter
   });
   if(top.location.search.indexOf("usuarioConSesion")>-1)  url3="http://olimpiadas.clarosports.com/mrss/video/clarosports.xml";//url3="http://noticias.prodigy.msn.com/rss-carrouselinfopane.aspx";//, //urlcon sesión
   else url = "http://olimpiadas.clarosports.com/mrss/video/clarosports.xml";//"http://prodigy.msn.com/rss-slideshow-telmex.aspx";
   $.ajax({
 		url:'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&output=json_xml&num=100&callback=?&q='+ encodeURIComponent(url),
 		dataType: 'json',
-		success: parseNivo
+		success: parseRight
   });
-
+  if(top.location.search.indexOf("usuarioConSesion")>-1) url = "http://estilos.prodigy.msn.com/rss-carrouselinfopane.aspx";//"http://prodigy.msn.com/rss-slideshow-telmex.aspx"; //Url con sesión
+  else url = "http://prodigy.msn.com/rss-slideshow-telmex.aspx"
 
   $.ajax({
-		url:"http://msn-rd.com/eservicios/json/bannerswg",
-		dataType: 'jsonp',
-		success: parseBanners
+		url:'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&output=json_xml&num=100&callback=?&q='+ encodeURIComponent(url),
+		dataType: 'json',
+		success: parseLeft
   });
 });
 
 
-function parsePane2(data) {
-	data.responseData.xmlString = data.responseData.xmlString.replace(/msncp\:/g, 'msncp');
-	var placeHolder = "ul.pane";
-	var items = data.responseData.feed.entries;
-	var xmlsrc = $(data.responseData.xmlString).find("item");
-	var li = $(placeHolder+" li");
-	$(placeHolder+" li").remove();
-	for(var j=0;j<items.length;j++){
-		var item = items[j];
-		var itemimage = $(xmlsrc[j]).find("enclosure").attr("url");
-		var newli = li.clone();
-		$(newli).find("h2").text(item.title);
-		$(newli).find("a").attr("href",item.link);
-		$(newli).find("img").attr("src",itemimage);
-		$(placeHolder).append(newli);
-	}
-	$("#slider-pane").als({
-		visible_items: 5,
-		scrolling_items: 4,
-		orientation: "horizontal",
-		circular: "no",
-		autoscroll: "no"
-	});
-}
 
-function parseEntries(data) {
+
+function parseCenter(data) {
 	data.responseData.xmlString = data.responseData.xmlString.replace(/msncp\:/g, 'msncp');
 	var placeHolder = "ul.slider";
 	var items = data.responseData.feed.entries;
@@ -75,7 +46,7 @@ function parseEntries(data) {
 		$(newli).find("img").attr("src",itemimage);
 		$(placeHolder).append(newli);
 	}
-	$("#slider-items").als({
+	$("#slider-center").als({
 		visible_items: 6,
 		scrolling_items: 6,
 		orientation: "horizontal",
@@ -84,29 +55,9 @@ function parseEntries(data) {
 	});
 }
 
-function parseBanners(data) {
-	
-	var placeHolder = "div.banners";
-	var items = data.items;
-	var li = $(placeHolder+" .li");
-	$(placeHolder+" .li").remove();
-	for(var j=0;j<items.length;j++){
-		var item = items[j];
-		var itemimage = item.php;
-		console.log(itemimage);
-		var newli = li.clone();
-		$(newli).find("a").attr("title",item.title);
-		$(newli).find("a").attr("href",item.link);
-		$(newli).find("img").attr("src",itemimage);
-		if(j==items.length-1) $(newli).addClass("last");
-		$(placeHolder).append(newli);
-	}
-	
-}
-
-function parseNivo(data) {
+function parseLeft(data) {
 	data.responseData.xmlString = data.responseData.xmlString.replace(/msncp\:/g, 'msncp');
-	var placeHolder = "#slider-nivo";
+	var placeHolder = "#slider-left";
 	var items = data.responseData.feed.entries;
 	var xmlsrc = $(data.responseData.xmlString).find("item");
 	var li = $(placeHolder+" .li");
@@ -131,16 +82,16 @@ function parseNivo(data) {
     });
 
 }
-function parsePane(data) {
+function parseRight(data) {
 	data.responseData.xmlString = data.responseData.xmlString.replace(/msncp\:/g, 'msncp');
-	var placeHolder = "#slider-pane";
+	var placeHolder = "#slider-right";
 	var items = data.responseData.feed.entries;
 	var xmlsrc = $(data.responseData.xmlString).find("item");
 	var li = $(placeHolder+" .li");
 	$(placeHolder+" .li").remove();
 	for(var j=items.length/2;j<items.length;j++){
 		var item = items[j];
-		var itemimage = $(xmlsrc[j]).find("enclosure").attr("url");
+		var itemimage = $(xmlsrc[j]).find("videoImage").attr("url");
 		var newli = li.clone();
 		$(newli).find("a").attr("href",item.link);
 		$(newli).find("img").attr("src",itemimage)
